6f03c7ffa24e72cc34e99ae9badc6d5a41b545af
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-30 14:52:46 -0500

    added in some node modules and gitnore

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const bcrypt = require("bcrypt-nodejs");
+const mysql = require("mysql");
+const config = require("../config");
+
+const helmet = require('helmet');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
