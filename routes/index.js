8bf83f993a7502059e13c3de528df9d8e93f9414
Greg Roques <greg.roques@gmail.com>
2019-01-31 11:18:56 -0500

    creating route and view pages

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+// custom middleware variables
+const config = require('config');
+const mysql = require('mysql');
+const connection = mysql.createConnection(config.db);
+  connection.connect();
+const bcrpt = require('bcrypt-node.js');
+const request = require('request');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
---
+++ routes/login.js
@@ -0,0 +1,58 @@
+// require express
+var express = require('express');
+var router = express.Router();
+
+// middleware
+const config = require('config');
+const mysql = require('mysql');
+const connection = mysql.createConnection(config.db);
+  connection.connect();
+const bcrpt = require('bcrypt-node.js');
+
+// Get Login
+app.get('/login', (req, res, next)=>{
+    let msg;
+    if(req.query.msg == 'noUser'){
+        msg = '<h2>This email is not registered in our system. Please try again or register!</h2>'
+    }else if(req.query.msg == 'badPass'){
+        msg = '<h2>This password is not associated with this email. Please enter again</h2>'
+    }
+	res.render('login',{msg});
+});
+
+
+app.post('/loginProcess',(req, res, next)=>{
+
+    const email =  req.body.email;
+    const password = req.body.password;
+
+    const checkPasswordQuery = `SELECT * FROM users WHERE email = ?`;
+    connection.query(checkPasswordQuery,[email],(error, results)=>{
+        if(error){throw error;}
+
+        if(results.length == 0 ){
+
+            res.redirect('/login?msg=noUser');
+        }else{
+
+            const passwordsMatch = bcrypt.compareSync(password,results[0].hash);
+            if(!passwordsMatch){
+
+                res.redirect('/login?msg=badPass');
+            }else{
+
+                console.log(results[0].id)
+                req.session.name = results[0].name;
+                req.session.email = results[0].email;
+
+                req.session.uid = results[0].id;
+                req.session.loggedIn = true;
+                res.redirect('/?msg=loginSuccess');
+
+            }
+        }
+    })
+});
+
+
+module.exports = router;
