e4b01cc2e1360741b56ce508d60688224e2ec8f5
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-31 21:34:11 -0500

    connected DB for userProfileInfo

--- routes/register.js
+++ routes/register.js
@@ -19,10 +19,11 @@ router.get('/',(req, res)=>{
     res.render('register',{msg});
 });
 
+let userId;
 // Return Registration
 router.post('/registerProcess',(req, res, next)=>{
     const hashedPass = bcrypt.hashSync(req.body.password);
-    console.log(hashedPass);
+    // console.log(hashedPass);
     const checkUserQuery = `SELECT * FROM loginInfo WHERE email = ?;`;
     connection.query(checkUserQuery,[req.body.email],(error,results)=>{
         if(error){throw error;}
@@ -34,10 +35,49 @@ router.post('/registerProcess',(req, res, next)=>{
             (?,?,?);`;
             connection.query(insertUserQuery,[req.body.userName, req.body.email, hashedPass],(error2, results2)=>{
                 if(error2){throw error2};
-                res.redirect('/?msg=regSuccess');
             });
+            const sessionQuery = `SELECT * FROM loginInfo WHERE email = ?;`;
+                connection.query(sessionQuery,[req.body.email],(error3,results3)=>{
+                    console.log(results3[0]);
+                    userId = results3[0].id;
+            });
+            res.redirect('/register/profileCreation');
         };
     });
 });
+
+
+router.get("/profileCreation", (req,res,next)=>{
+    res.render("profileCreation", {});
+  })
+  
+  router.post("/userProfileCreation", (req,res,next)=>{
+    userId;
+    const firstName = req.body.firstName;
+    const age = req.body.age;
+    const sex = req.body.sex;
+  
+  // change height from inches to cm for purpose of db/api usage
+    let heightFeet = parseInt(req.body.heightFeet);
+    let heightInches = parseInt(req.body.heightInches);
+    let heightTotalInches = (heightFeet * 12) + heightInches;
+  
+    const heightTotalCm = heightTotalInches * 2.54;
+  
+  // change weight from pounds to kilograms
+    let startingWeightLb = parseInt(req.body.startingWeight);
+    const startingWeightKg = startingWeightLb * 0.453592;
+    // console.log(startingWeightKg);
+    let targetWeightLb = parseInt(req.body.targetWeight);
+    const targetWeightKg = targetWeightLb * 0.453592;
+    // console.log(targetWeightKg);
+
+    const insertUserQuery = `INSERT INTO userProfileInfo (firstName, sex, height, startingWeight, age, targetWeight, userId)
+        VALUES
+        (?,?,?,?,?,?,?);`;
+        connection.query(insertUserQuery,[firstName, sex, heightTotalCm, startingWeightKg, age, targetWeightKg, userId],(error, results)=>{
+            if(error){throw error};
+    });
+  });
 
 module.exports = router;
